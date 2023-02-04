55dc173f70801095144ca0e6b2768390aa196bd6
a8a3 <alexpoligon@mail.ru>
2023-02-04 20:03:08 +0100

    http log parser. statistics calculation

---
+++ http_parser/include/http_record.h
@@ -0,0 +1,112 @@
+#pragma once
+
+#include <assert.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define URL_DEF_SZ 256
+#define REF_DEF_SZ 256
+#
+
+typedef struct {
+    char* url;
+    size_t url_sz;
+    char* referer;
+    size_t referer_sz;
+    size_t bytes;
+} http_record;
+
+void http_record_remove(http_record** record) {
+    free((*record)->url);
+    free((*record)->referer);
+    free(*record);
+    *record = NULL;
+}
+
+http_record* http_rec_create(void) {
+    http_record* rec = malloc(sizeof(http_record));
+    assert(rec);
+
+    rec->url = malloc(URL_DEF_SZ);
+    assert(rec->url);
+    memset(rec->url, 0, URL_DEF_SZ);
+    rec->url_sz = URL_DEF_SZ;
+
+    rec->referer = malloc(REF_DEF_SZ);
+    assert(rec->referer);
+    memset(rec->referer, 0, REF_DEF_SZ);
+    rec->referer_sz = REF_DEF_SZ;
+
+    rec->bytes = 0;
+    return rec;
+}
+
+// return: 0 - happy camper
+//       : 1 - error
+int http_record_get(char* line, http_record** out_rec) {
+    if (NULL == *out_rec) {
+        *out_rec = http_rec_create();
+        assert(*out_rec);
+    }
+    http_record* rec = *out_rec;
+#define CHECK_TOKEN(chr, str)                                                                                          \
+    do {                                                                                                               \
+        if (NULL == chr) {                                                                                             \
+            return 1;                                                                                                  \
+        }                                                                                                              \
+    } while (0)
+    char* url_begin = strchr(line, '"');
+    CHECK_TOKEN(url_begin, line);
+
+    char* url_val_begin = strchr(url_begin, '/');
+    CHECK_TOKEN(url_val_begin, line);
+
+    char* url_val_end = strchr(url_val_begin, ' ');
+    CHECK_TOKEN(url_val_end, line);
+
+    size_t url_val_sz = url_val_end - url_val_begin;
+
+    if (url_val_sz > rec->url_sz) {
+        rec->url = realloc(rec->url, url_val_sz + 1);
+        assert(rec->url);
+    }
+    strncpy(rec->url, url_val_begin, url_val_sz);
+    rec->url[url_val_sz] = '\0';
+    rec->url_sz = url_val_sz;
+
+    char* status_begin = strchr(url_val_end + 1, ' ');
+    CHECK_TOKEN(status_begin, line);
+
+    char* end_ptr;
+    char* size_begin = strchr(status_begin + 1, ' ');
+    CHECK_TOKEN(size_begin, line);
+
+    rec->bytes = strtoul(size_begin + 1, &end_ptr, 10);
+
+    char* referer_begin = strchr(size_begin + 1, '"');
+    CHECK_TOKEN(referer_begin, line);
+
+    ++referer_begin;
+
+    char* referer_end = strchr(referer_begin, '"');
+    CHECK_TOKEN(referer_end, line);
+
+    size_t referer_val_sz = referer_end - referer_begin;
+    if (0 == strncmp(referer_begin, "-", referer_val_sz)) {
+        return 0;
+    }
+
+    if (referer_val_sz > rec->referer_sz) {
+        rec->referer = realloc(rec->referer, referer_val_sz + 1);
+        assert(rec->referer);
+    }
+
+    strncpy(rec->referer, referer_begin, referer_val_sz);
+    rec->referer[referer_val_sz] = '\0';
+    rec->referer_sz = referer_val_sz;
+    *out_rec = rec;
+    return 0;
+}
--- http_parser/src/main.c
+++ http_parser/src/main.c
@@ -1,16 +1,26 @@
+#include <linux/limits.h>
 #define _DEFAULT_SOURCE
 #define _POSIX_C_SOURCE 200809L
 
 #include <assert.h>
 #include <dirent.h>
 #include <getopt.h>
+#include <pthread.h>
+#include <stdatomic.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <threads.h>
 
 #include <hash_map.h>
+#include <http_record.h>
 
 #define FILES_LIST_INIT_SZ 8
+#define HASH_MAP_INIT_SZ 1024
+#define URLS_STAT_SZ 10
+#define REFS_STAT_SZ 10
+
+#define MIN(a, b) (a) < (b) ? (a) : (b)
 
 void print_help(void) {
     printf("NAME:\n"
@@ -25,14 +35,26 @@ void print_help(void) {
            "        Print help\n");
 }
 
-char** create_files_list(char* dir_name) {
+typedef char** _Atomic atomic_iter;
+
+char** files_list_create(char* dir_name, size_t* files_count) {
     DIR* dp = opendir(dir_name);
     if (NULL == dp) {
-        perror("opendir");
+        perror(dir_name);
         exit(EXIT_FAILURE);
     }
+// C standard doesn't guarantee that all-bits-zero is the NULL pointer constant,
+// explicitly nullify each pointer instead zeroing by memset
+#define NULLIFY_PTRS(ptrs_arr, ptrs_arr_sz)                                                                            \
+    do {                                                                                                               \
+        for (size_t i = 0; i < (ptrs_arr_sz); ++i)                                                                     \
+            (ptrs_arr)[i] = NULL;                                                                                      \
+    } while (false)
+
     size_t files_list_sz = FILES_LIST_INIT_SZ;
-    char** res = (char**)malloc((files_list_sz + 1) * sizeof(char*));
+    char** res = malloc((files_list_sz + 1) * sizeof(char*));
+    assert(res);
+    NULLIFY_PTRS(res, files_list_sz + 1);
     size_t f_idx = 0;
     size_t entry_pos;
     struct dirent* entry;
@@ -61,58 +83,51 @@ char** create_files_list(char* dir_name) {
 
             if (f_idx >= files_list_sz) {
                 files_list_sz *= 2;
-                res = (char**)realloc(res, (files_list_sz + 1) * sizeof(char*));
+                res = realloc(res, (files_list_sz + 1) * sizeof(char*));
                 assert(res);
+                NULLIFY_PTRS(res + f_idx, files_list_sz + 1 - f_idx);
             }
         }
     }
     closedir(dp);
     res[f_idx] = NULL;
+    *files_count = f_idx;
     return res;
 }
 
-void remove_files_list(char** files_list) {
-    char** files_iter = files_list;
+void files_list_remove(atomic_iter files_list) {
+    char** iter = files_list;
     char* file;
-    while ((file = *(files_iter++))) {
+    while ((file = *(iter++))) {
         free(file);
     }
     free(files_list);
     files_list = NULL;
 }
 
-void parse_line(char* line) {
-    char* url_begin = strchr(line, '"');
-    char* url_val_begin = strchr(url_begin, '/');
-    printf("%s\n", url_val_begin);
-    char* url_val_end = strchr(url_val_begin, ' ');
-    size_t url_val_sz = url_val_end - url_val_begin;
+typedef struct {
+    hash_map* url;
+    hash_map* referer;
+    atomic_iter* files_iter;
+} worker_param;
 
-    char* url_val = (char*)malloc(url_val_sz + 1);
-    strncpy(url_val, url_val_begin, url_val_sz);
-    url_val[url_val_sz] = '\0';
-    printf("url    : [%s]\n", url_val);
-
-    char* status_begin = strchr(url_val_end + 1, ' ');
-    char* size_begin = strchr(status_begin + 1, ' ');
-    char* end_ptr;
-    long size = strtol(size_begin + 1, &end_ptr, 10);
-    printf("size   : [%ld]\n", size);
-
-    char* referer_begin = strchr(size_begin + 1, '"');
-    ++referer_begin;
-    char* referer_end = strchr(referer_begin + 1, '"');
-    size_t referer_val_sz = referer_end - referer_begin;
-
-    char* referer_val = (char*)malloc(referer_val_sz + 1);
-    strncpy(referer_val, referer_begin, referer_val_sz);
-    referer_val[referer_val_sz] = '\0';
-    printf("referer: [%s]\n", referer_val);
-    free(url_val);
-    free(referer_val);
+worker_param* worker_param_create(atomic_iter* files_iter) {
+    worker_param* wp = (worker_param*)malloc(sizeof(worker_param));
+    wp->url = create_hash_map(HASH_MAP_INIT_SZ);
+    wp->referer = create_hash_map(HASH_MAP_INIT_SZ);
+    wp->files_iter = files_iter;
+    return wp;
 }
 
-void read_file_data(char* file_name) {
+void worker_param_remove(worker_param** wp) {
+    remove_hash_map(&(*wp)->url);
+    remove_hash_map(&(*wp)->referer);
+    (*wp)->files_iter = NULL;
+    free(*wp);
+    wp = NULL;
+}
+
+void file_data_read(char* file_name, hash_map* url, hash_map* referer) {
     FILE* fd = fopen(file_name, "r");
     if (NULL == fd) {
         perror(file_name);
@@ -122,34 +137,133 @@ void read_file_data(char* file_name) {
     ssize_t read;
     size_t len;
     char* line = NULL;
+    http_record* record = NULL;
     while (-1 != (read = getline(&line, &len, fd))) {
-        // parse_line(line);
-    }
-    char* test_line = "52.28.236.88 - - [22/Jul/2020:23:33:53 +0000] \"GET "
-                      "/.well-known/acme-challenge/H0bmB07yr_tHL1xbs_NTouoDBIQnRXm405Kbjx61hjg HTTP/1.1\" 200 87 "
-                      "\"http://baneks.site/.well-known/acme-challenge/"
-                      "H0bmB07yr_tHL1xbs_NTouoDBIQnRXm405Kbjx61hjg\" \"Mozilla/5.0 (compatible; Let's Encrypt "
-                      "validation server; +https://www.letsencrypt.org)"
-                      "-";
-    parse_line(test_line);
+        if (1 == http_record_get(line, &record) || NULL == record)
+            continue;
 
+        if (record->url_sz > 0) {
+            node* url_n = hash_map_insert(url, record->url, record->url_sz);
+            assert(url_n);
+            url_n->val += record->bytes;
+        }
+
+        if (record->referer_sz > 0) {
+            node* ref_n = hash_map_insert(referer, record->referer, record->referer_sz);
+            assert(ref_n);
+            ref_n->val += 1;
+        }
+    }
+    http_record_remove(&record);
     free(line);
     fclose(fd);
 }
 
-void parse(char* dir_name, int num_threads) {
-    char** files_list = create_files_list(dir_name);
+int worker_func(void* data) {
+    worker_param* wp = (worker_param*)data;
+    char* file;
+    // it doesn't work-> *atomic_fetch_add_explicit(wp->files_iter, 1, memory_order_acq_rel)
+    while ((file = *(*wp->files_iter)++)) {
+        printf("%s file is being processed by %lu thread\n", file, pthread_self());
+        file_data_read(file, wp->url, wp->referer);
+    }
+    thrd_exit(0);
+}
+
+void hash_map_add(hash_map* dst, hash_map* src) {
+    node* n = NULL;
+    for (size_t i = 0; i < src->capacity; ++i) {
+        n = src->nodes + i;
+
+        if (NULL != n->key) {
+            node* added = hash_map_insert(dst, n->key, n->key_len);
+            assert(NULL != added);
+            added->val += n->val;
+        }
+    }
+}
+
+int nodes_desc_cmp(const void* p1, const void* p2) {
+    node* n1 = *(node**)p1;
+    node* n2 = *(node**)p2;
+
+    if (n1->val < n2->val)
+        return 1;
+    if (n1->val > n2->val)
+        return -1;
+
+    return 0;
+}
+
+typedef node** nodes_arr;
+void create_nodes_sorted_arr(hash_map* m, nodes_arr* arr, size_t* arr_sz) {
+    node** node_ptr_arr = malloc(m->size * sizeof(node*));
+    size_t arr_idx = 0;
+    for (size_t i = 0; i < m->capacity; ++i) {
+        node* n = m->nodes + i;
+        if (NULL != n->key) {
+            node_ptr_arr[arr_idx++] = n;
+        }
+    }
+    qsort(node_ptr_arr, m->size, sizeof(node*), nodes_desc_cmp);
+    *arr = node_ptr_arr;
+    *arr_sz = m->size;
+}
+
+void get_stat(char* dir_name, int num_threads) {
+    size_t files_count = 0;
+    char** files_list = files_list_create(dir_name, &files_count);
     if (NULL == files_list[0]) {
         perror("directory is empty\n");
         exit(EXIT_FAILURE);
     }
-    char** files_iter = files_list;
-    char* file;
-    while ((file = *(files_iter++))) {
-        read_file_data(file);
-        ++file;
+    atomic_iter files_iter = files_list;
+    num_threads = MIN((size_t)num_threads, files_count);
+
+    worker_param** params = malloc(sizeof(worker_param*) * num_threads);
+    pthread_t* workers = (pthread_t*)malloc(sizeof(pthread_t) * num_threads);
+    for (int i = 0; i < num_threads; ++i) {
+        params[i] = worker_param_create(&files_iter);
+        thrd_create(workers + i, worker_func, params[i]);
     }
-    remove_files_list(files_list);
+
+    thrd_join(workers[0], NULL);
+    hash_map* url_result = params[0]->url;
+    hash_map* ref_result = params[0]->referer;
+
+    for (int i = 1; i < num_threads; ++i) {
+        thrd_join(workers[i], NULL);
+        hash_map_add(url_result, params[i]->url);
+        hash_map_add(ref_result, params[i]->referer);
+    }
+    printf("\nstatistics calculation...\n");
+    // hash_maps -> sorted arrays
+    nodes_arr arr;
+    size_t sz;
+    create_nodes_sorted_arr(url_result, &arr, &sz);
+    sz = MIN(sz, URLS_STAT_SZ);
+
+    printf("\nMost trafic URLs:\n");
+    for (size_t i = 0; i < sz; ++i) {
+        printf("%lu) BYTES: [%d], URL: [%s]\n", i, arr[i]->val, arr[i]->key);
+    }
+    free(arr);
+
+    create_nodes_sorted_arr(ref_result, &arr, &sz);
+    sz = MIN(sz, REFS_STAT_SZ);
+
+    printf("\nMost used REFERERs:\n");
+    for (size_t i = 0; i < sz; ++i) {
+        printf("%lu) TIMES: [%d], REFERER: [%s]\n", i, arr[i]->val, arr[i]->key);
+    }
+    free(arr);
+
+    for (int i = 0; i < num_threads; ++i) {
+        worker_param_remove(&params[i]);
+    }
+    free(params);
+    free(workers);
+    files_list_remove(files_list);
 }
 
 int main(int argc, char** argv) {
@@ -172,7 +286,7 @@ int main(int argc, char** argv) {
             case 't': {
                 num_threads = atoi(optarg);
                 if (0 == num_threads) {
-                    fprintf(stderr, "unexpected threads number passed: %s", optarg);
+                    fprintf(stderr, "incorrect threads number passed: %s", optarg);
                     exit(EXIT_FAILURE);
                 }
                 break;
@@ -193,6 +307,6 @@ int main(int argc, char** argv) {
             }
         }
     }
-    parse(dir_name, num_threads);
+    get_stat(dir_name, num_threads);
     exit(EXIT_SUCCESS);
 }
