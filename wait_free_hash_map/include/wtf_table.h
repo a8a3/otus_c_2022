a389e56a3bb72eb5fe8ba8e04bf5fefca223861b
a8a3 <alexpoligon@mail.ru>
2023-03-27 00:21:07 +0200

    wtf_table. nodes marking functionality

--- wait_free_hash_map/include/wtf_table.h
+++ wait_free_hash_map/include/wtf_table.h
@@ -12,7 +12,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BITS_COUNT(entity) sizeof((entity)) * (CHAR_BIT)
+#define BITS_COUNT(entity) sizeof(entity) * CHAR_BIT
+typedef size_t hash_t;
 
 typedef struct {
     size_t hash_;
@@ -21,101 +22,167 @@ typedef struct {
 typedef node_t* node_ptr_t;
 typedef _Atomic(node_t*) atomic_node_ptr_t; // i.e atomic pointer to non-atomic node_t
 
-// TODO: do I need atomics in is_... funcs?
-// node is array if 2nd LSB is turned on
-
-// mark LSB
-void mark_data_node(atomic_uintptr_t* n) { atomic_fetch_or_explicit(n, (uintptr_t)1U, memory_order_release); }
-
-// return value with unmarked LSB
-atomic_uintptr_t unmark_data_node(atomic_uintptr_t n) {
-    // TODO: do I need atomic operation here?
-    atomic_fetch_and_explicit(&n, ~((uintptr_t)1U), memory_order_release);
-    return n;
+void mark_atomic_node(atomic_node_ptr_t* n) {
+    atomic_fetch_or_explicit((atomic_uintptr_t*)n, (uintptr_t)1U, memory_order_release);
 }
 
-// node is marked if LSB is turned on
-bool is_marked_node(atomic_uintptr_t n) { return n & 1U; }
-bool is_array_node(atomic_uintptr_t n) { return n & (1U << 1); }
+void unmark_atomic_node(atomic_node_ptr_t* n) {
+    atomic_fetch_and_explicit((atomic_uintptr_t*)n, ~((uintptr_t)1U), memory_order_release);
+}
 
-typedef union {
-    atomic_node_ptr_t as_ptr;
-    atomic_uintptr_t as_uint;
-} atomic_ptr_uint_u;
+node_ptr_t* get_unmarked_node(atomic_node_ptr_t* n) {
+    return (node_ptr_t*)((uintptr_t)atomic_load_explicit(n, memory_order_acquire) & ~(uintptr_t)1U); // NOLINT
+}
 
-typedef union {
-    node_ptr_t as_ptr;
-    uintptr_t as_uint;
-} ptr_uint_u;
+bool is_node_marked(node_ptr_t* n) { return (uintptr_t)n & 1U; }
+
+bool is_atomic_node_marked(atomic_node_ptr_t* n) {
+    return (uintptr_t)atomic_load_explicit(n, memory_order_acquire) & 1U;
+}
+
+bool is_atomic_array(atomic_node_ptr_t* a) {
+    return (uintptr_t)atomic_load_explicit(a, memory_order_acquire) & (1U << 1);
+}
+
+void mark_as_array(void** a) {
+    *a = (uintptr_t*)((uintptr_t)*a | (uintptr_t)1U << 1); // NOLINT
+}
+
+// unmark 2nd LSB
+void unmark_array(void** a) {
+    *a = (uintptr_t*)((uintptr_t)*a & ~((uintptr_t)1U << 1)); // NOLINT
+}
+
+// return value with unmarked 2nd LSB
+void* get_unmarked_array(void* a) {
+    return (void*)((uintptr_t)a & ~((uintptr_t)1U << 1)); // NOLINT
+}
+
+bool is_array(void* a) { return (uintptr_t)a & ((uintptr_t)1U << 1); }
 
 // wtf stands for WaiT Free
 typedef struct {
-    atomic_ptr_uint_u* head_;
-    size_t head_sz_; // head and all array nodes have same size
+    atomic_node_ptr_t* head_;
+    size_t arr_sz_; // head and all array nodes have same size
     size_t max_fail_count;
+    uint8_t bits_shift;
 } wtf_table_t;
 
 // Create table instance and return pointer on it
-wtf_table_t* wtf_table_create(size_t head_sz) {
+wtf_table_t* wtf_table_create(size_t arr_sz) {
     wtf_table_t* t = malloc(sizeof(wtf_table_t));
-    t->head_ = malloc(sizeof(atomic_ptr_uint_u) * head_sz);
-    t->head_sz_ = head_sz;
-    memset(t->head_, 0, sizeof(atomic_ptr_uint_u) * head_sz);
+    t->head_ = malloc(sizeof(atomic_node_ptr_t) * arr_sz);
+    t->arr_sz_ = arr_sz;
+    memset(t->head_, 0, sizeof(atomic_node_ptr_t) * arr_sz);
     // TODO: magic number
     t->max_fail_count = 10;
+    t->bits_shift = (uint8_t)log2(BITS_COUNT(hash_t));
     return t;
 }
 
 node_t* allocate_node(size_t hash, const void* value) {
     node_t* n = malloc(sizeof(node_t));
+    if (NULL == n) {
+        return NULL;
+    }
     n->hash_ = hash;
     n->value_ = value;
     return n;
 }
 
+// replace node value with new array
+// insert value into new array
+// return new array to caller side
+// atomic_ptr_uint_u* expand_table(wtf_table_t* t, atomic_ptr_uint_u* expanded_node, size_t r) {
+//    uintptr_t node = atomic_load_explicit(&expanded_node->as_uint, memory_order_acquire);
+//    atomic_ptr_uint_u* new_array = malloc(sizeof(atomic_ptr_uint_u) * t->arr_sz_);
+//    memset(new_array, 0, sizeof(atomic_ptr_uint_u) * t->arr_sz_);
+//    printf("exp: new array allocated: %p\n", new_array);
+//
+//    // calc expanded node idx in new array
+//    uint8_t expanded_node_new_pos = (expanded_node->as_ptr->hash_ >> (t->bits_shift + r)) & (t->arr_sz_ - 1);
+//    printf("exp: expanded node new pos: %u\n", expanded_node_new_pos);
+//    new_array[expanded_node_new_pos].as_ptr = expanded_node->as_ptr;
+//
+//    mark_as_array((void**)&new_array);
+//    printf("exp: new array is marked: %p\n", new_array);
+//
+//    // replace node with array
+//    if (atomic_compare_exchange_strong(&expanded_node->as_uint, &node, (uintptr_t)new_array)) {
+//        return new_array;
+//    } else {
+//        // other thread has inserted array in this node already
+//        if (is_array_node(&expanded_node->as_uint)) {
+//            // the node was already expanded, free stuff created above
+//            free(new_array);
+//            return expanded_node;
+//        }
+//    }
+//    return NULL;
+//}
+
 // Insert key-value pair it table.
 // here key is precalculated hash of a real key
 // If the table holds the key, value overwrites the previous value, and wtf_table_insert returns the previous value.
 // Otherwise, key value pair is added to the table, and wtf_table_insert returns NULL
-void* wtf_table_insert(wtf_table_t* t, size_t hash, const void* value) {
-    ptr_uint_u new_node;
-    new_node.as_ptr = allocate_node(hash, value);
-    // TODO: handle malloc error
-    atomic_ptr_uint_u* local = t->head_; // start from head
+void* wtf_table_insert(wtf_table_t* t, hash_t hash, const void* value) {
+    atomic_node_ptr_t node, new_node;
+    new_node = allocate_node(hash, value);
+    printf("ins: node allocated at %p\n", new_node);
 
-    uint8_t bits_shift = (uint8_t)log2(BITS_COUNT(hash)), fail_count = 0;
-    for (size_t r = 0; r < BITS_COUNT(hash); r += bits_shift) {
+    if (NULL == new_node) {
+        return NULL;
+    }
+    atomic_node_ptr_t* local = t->head_; // start from head
+    uint8_t fail_count = 0;
+
+    for (size_t r = 0; r < BITS_COUNT(hash); r += t->bits_shift) {
         // TODO: 10th r should be 4 bits length
-        uint8_t pos = hash & (t->head_sz_ - 1); // position in local(current) array node
-        printf("insert: pos in local: %u\n", pos);
-        hash >>= bits_shift;
+        uint8_t pos = hash & (t->arr_sz_ - 1); // position in local(current) array node
+        printf("ins: hash: %zu, pos in local: %u\n", hash, pos);
+        hash >>= t->bits_shift;
         fail_count = 0;
 
         for (;;) {
             if (fail_count > t->max_fail_count) {
-                mark_data_node(&local[pos].as_uint);
+                mark_atomic_node(&local[pos]);
             }
+            node = local[pos];
 
-            atomic_ptr_uint_u node = local[pos];
-            if (is_array_node(node.as_uint)) {
-                local = &node;
+            printf("ins: candidate at %u pos is %p\n", pos, node);
+            if (is_array(&node)) {
+                local = get_unmarked_array(node);
                 break; // for
-            } else if (is_marked_node(node.as_uint)) {
+            } else if (is_atomic_node_marked(&node)) {
                 // TODO:   local = expand_table(local, pos, node, r);
                 break;
-            } else if (NULL == node.as_ptr) {
+            } else if (NULL == node) {
                 // TODO: explicit cmp func
-                if (atomic_compare_exchange_strong(&local[pos].as_uint, (uintptr_t*)&node.as_uint, new_node.as_uint)) {
+                if (atomic_compare_exchange_strong((atomic_uintptr_t*)local[pos], (uintptr_t*)node,
+                                                   (uintptr_t)new_node)) {
+                    printf("ins: %p is inserted at %u, level: %lu\n", new_node, pos, r / t->bits_shift);
                     return NULL; // node added
                 } else {
                     // TBD
                 }
             } else {
                 // do I need atomic pointer here?
-                if (((node_ptr_t)(node.as_ptr))->hash_ == new_node.as_ptr->hash_) {
+                if (node->hash_ == new_node->hash_) {
 
                 } else {
-                    // TODO:   local = expand_table(local, pos, node, r);
+                    //                    printf("ins: expand %p\n", local);
+                    //                    atomic_node_ptr_t* expanded = expand_table(t, &local[pos], r);
+                    //                    printf("ins: expanded %p, in table %p\n", expanded, local[pos]);
+                    //
+                    //                    if (!is_array(expanded)) {
+                    //                        ++fail_count;
+                    //                    } else {
+                    //                        //                    unmark_array_node_ex((void**)&expanded);
+                    //                        //                    printf("ins: unmarked expanded %p\n", expanded);
+                    //                        local = expanded;
+                    //                        printf("ins: set local to %p\n", local);
+                    //                        break; // inner for
+                    //                    }
                 }
             }
         }
@@ -125,25 +192,36 @@ void* wtf_table_insert(wtf_table_t* t, size_t hash, const void* value) {
 
 // Return value associated with the key, if the table doesn't hold the key, return NULL
 const void* wtf_table_find(wtf_table_t* t, size_t hash) {
-    atomic_ptr_uint_u* local = t->head_;
+    atomic_node_ptr_t* local = t->head_;
+    node_ptr_t* node;
+
     size_t local_hash = hash;
-    uint8_t bits_shift = (uint8_t)log2(BITS_COUNT(hash)), pos;
-    for (size_t r = 0; r < BITS_COUNT(hash); r += bits_shift) {
-        pos = local_hash & (t->head_sz_ - 1);
-        printf("find: pos in local: %u\n", pos);
-        local_hash >>= bits_shift;
-        atomic_ptr_uint_u node;
-        node.as_uint = unmark_data_node(local[pos].as_uint);
-        if (is_array_node(node.as_uint)) {
-            printf("find: array node in pos: %u\n", pos);
-            local = &node;
+    uint8_t pos;
+    printf("fnd: ==> find %lu hash\n", hash);
+
+    for (size_t r = 0; r < BITS_COUNT(hash); r += t->bits_shift) {
+        pos = local_hash & (t->arr_sz_ - 1);
+        // printf("fnd: pos in local: %u\n", pos);
+        local_hash >>= t->bits_shift;
+        // printf("fnd: attempt to unmark node: %p\n", local[pos].as_ptr);
+        node = get_unmarked_node(&local[pos]);
+        // printf("fnd: unmarked: %p\n", node.as_ptr);
+
+        if (is_array(node)) {
+            printf("fnd: array node %p in pos: %u, level: %lu\n", node, pos, r / t->bits_shift);
+            local = &local[pos];
+            unmark_array((void**)&local);
+            printf("fnd: unmarked array: %p\n", local);
         } else {
-            printf("find: regular node in pos: %u\n", pos);
-            if (node.as_ptr->hash_ == hash) {
-                printf("find: regular node in pos: %u is found\n", pos);
-                return node.as_ptr->value_;
+            printf("fnd: regular node %p in pos: %u, level: %lu\n", node, pos, r / t->bits_shift);
+            if (NULL == node) {
+                printf("fnd: node is empty\n");
+                return NULL;
+            } else if ((*node)->hash_ == hash) {
+                printf("fnd: hash matched: %lu is found\n", hash);
+                return (*node)->value_;
             } else {
-                printf("find: hash mismatched: requested: %lu, found: %lu\n", hash, node.as_ptr->hash_);
+                printf("fnd: hash mismatched: requested: %lu, found: %lu\n", hash, (*node)->hash_);
                 return NULL;
             }
         }
--- wait_free_hash_map/tests/wtf_table_ut.c
+++ wait_free_hash_map/tests/wtf_table_ut.c
@@ -76,19 +76,46 @@ static void wtf_table_atomic_statements(void** state) { // NOLINT
 }
 
 static void wtf_table_nodes_marking(void** state) { // NOLINT
-    uintptr_t initial_val = 0xFFFFFFFC;
-    atomic_uintptr_t val = initial_val;
-    assert_false(is_marked_node(val));
 
-    mark_data_node(&val);
-    assert_true(is_marked_node(val));
-    assert_false(val == initial_val);
+    { // non-atomic array marking
+        print_message("marking via void ptr convertion\n");
+        int* new_int = malloc(sizeof(int));
+        print_message(" initial arr: %p\n", new_int);
+        assert_false(is_array(new_int));
+        mark_as_array((void**)&new_int);
+        print_message("  marked arr: %p\n", new_int);
+        assert_true(is_array(new_int));
 
-    atomic_uintptr_t unmarked = unmark_data_node(val);
-    assert_false(is_marked_node(unmarked));
+        int* ptr = get_unmarked_array(new_int);
+        print_message("unmarked arr: %p\n", ptr);
+        assert_false(is_array(ptr));
+        assert_true(is_array(new_int));
 
-    // rest bits are fine as well
-    assert_true(unmarked == initial_val);
+        unmark_array((void**)&new_int);
+        print_message("unmarked arr: %p\n", new_int);
+        assert_false(is_array(new_int));
+        free(new_int);
+    }
+
+    { // atomic node marking
+        atomic_node_ptr_t atomic_node = malloc(sizeof(node_t));
+        print_message(" initial atomic node: %p\n", atomic_node);
+        assert_false(is_atomic_node_marked(&atomic_node));
+        mark_atomic_node(&atomic_node);
+        print_message("  marked atomic node: %p\n", atomic_node);
+        assert_true(is_atomic_node_marked(&atomic_node));
+
+        node_ptr_t* tmp = get_unmarked_node(&atomic_node);
+        print_message("unmarked atomic node: %p\n", tmp);
+        assert_false(is_node_marked(tmp));
+        // origin is still marked
+        assert_true(is_atomic_node_marked(&atomic_node));
+
+        unmark_atomic_node(&atomic_node);
+        assert_false(is_atomic_node_marked(&atomic_node));
+        print_message("unmarked atomic node: %p\n", atomic_node);
+        free(atomic_node);
+    }
 }
 
 static void wtf_table_creation_deletion(void** state) { // NOLINT
@@ -98,12 +125,12 @@ static void wtf_table_creation_deletion(void** state) { // NOLINT
     assert_ptr_equal(t, NULL);
 }
 
-static void wtf_table_insertion(void** state) { // NOLINT
+static void wtf_table_simple_insert_find(void** state) { // NOLINT
     wtf_table_t* t = wtf_table_create(64);
-    const void* res = wtf_table_insert(t, 0, "value");
+    const void* res = wtf_table_insert(t, 0, "value0");
     assert_ptr_equal(res, NULL);
     res = wtf_table_find(t, 0);
-    assert_string_equal(res, "value");
+    assert_string_equal(res, "value0");
     res = wtf_table_insert(t, 1, "value1");
     res = wtf_table_insert(t, 2, "value2");
     res = wtf_table_insert(t, 3, "value3");
@@ -121,10 +148,36 @@ static void wtf_table_insertion(void** state) { // NOLINT
     wtf_table_destroy(&t);
 }
 
+static void wtf_table_insert_find_in_subarrays(void** state) { // NOLINT
+    wtf_table_t* t = wtf_table_create(64);
+    const void* res = wtf_table_insert(t, 0, "value0");
+    assert_ptr_equal(res, NULL);
+    // find first node in initial position
+    //    res = wtf_table_find(t, 0);
+    //    assert_string_equal(res, "value0");
+
+    // 64 has same same 6 LSBs as 0 -> subarray creation expected
+    res = wtf_table_insert(t, 64, "value64");
+    assert_ptr_equal(res, NULL);
+
+    // find value in subarray
+    res = wtf_table_find(t, 64);
+    assert_string_equal(res, "value64");
+
+    // find first node in new position on level 1
+    res = wtf_table_find(t, 0);
+    assert_string_equal(res, "value0");
+    wtf_table_destroy(&t);
+}
+
 int main(void) {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(wtf_table_statements), cmocka_unit_test(wtf_table_atomic_statements),
-        cmocka_unit_test(wtf_table_nodes_marking), cmocka_unit_test(wtf_table_creation_deletion),
-        cmocka_unit_test(wtf_table_insertion)};
+        cmocka_unit_test(wtf_table_statements),
+        //  cmocka_unit_test(wtf_table_atomic_statements),
+        cmocka_unit_test(wtf_table_nodes_marking),
+        // cmocka_unit_test(wtf_table_creation_deletion),
+        // cmocka_unit_test(wtf_table_simple_insert_find),
+        // cmocka_unit_test(wtf_table_insert_find_in_subarrays)
+    };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
